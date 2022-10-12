8ed76dad29f6395e3a42ed82ca79f1e810aaf21c
imstar15 <imstar15@qq.com>
2022-10-12 11:41:03 +0800

    TEST

--- src/bot/actions.js
+++ src/bot/actions.js
@@ -59,69 +59,7 @@ const drip = async (sender, address) => {
   
   const { amount } = dripActions[DRIP_TYPE.NORMAL];
 
-  return `I just sent ${amount} ${tokenSymbol} to address ${address}. Extrinsic hash: ${res.data.hash}.\n\nTry out our recurring payment feature on https://ace.web3go.xyz! (Click on Connect at the top right corner to select Turing Staging network)`;
-}
-
-const dripLater = async (sender, address, time) => {
-	if (_.isEmpty(address)) {
-    return 'please enter a wallet address after !drip-later.';
-  }
-
-  if (!isValidAddress(address)) {
-    return `The address ${address} entered is incompatible to ${networkName}.`;
-  }
-
-  const dripTime = parseTime(time);
-  if (!dripTime) {
-    return `Please enter the specified time format(UTC). Example: !drip-later address ${getNextHourStr()}`;
-  }
-  if (dripTime.isBefore(moment())) {
-    return "The time in UTC must be in the future.";
-  }
-  if (dripTime.isAfter(moment().add(maxScheduleSeconds, 'seconds'))) {
-    return `The time in UTC cannot be farther than ${moment.duration(maxScheduleSeconds, 'seconds').asDays()} days.`;
-  }
-
-  const res = await requestDrip({
-    sender,
-    address,
-    dripType: DRIP_TYPE.LATER,
-    dripTime: dripTime.valueOf(),
-  });
-
-  if (res.data === 'LIMIT') {
-    return `Your Discord ID or the address has reached its daily quota. Please request 3 times every 24 hours.`;
-  }
-  
-  const { amount } = dripActions[DRIP_TYPE.LATER];
-  const { data: { hash, providerId } } = res;
-
-  return `I will send ${amount} ${tokenSymbol} to address ${address} at ${time} UTC. Extrinsic hash: ${hash}. Your provided_id: ${providerId}.`;
-}
-
-const dripSwag = async (sender, address) => {
-	if (_.isEmpty(address)) {
-    return 'please enter a wallet address after !drip-swag.';
-  }
-
-  if (!isValidAddress(address)) {
-    return `The address ${address} entered is incompatible to ${networkName}.`;
-  }
-
-  const res = await requestDrip({
-    sender,
-    address,
-    dripType: DRIP_TYPE.SWAG,
-  });
-
-  if (res.data === 'LIMIT') {
-    return `Your Discord ID or the address has reached its daily quota. Please request only once every 24 hours.`;
-  }
-
-  const { amount } = dripActions[DRIP_TYPE.SWAG];
-  const { data: { hash, providerId } } = res;
-
-  return `For the next 24 hours, I will send ${amount} ${tokenSymbol} to address ${address} per hour. Extrinsic hash: ${hash}. Your provided_id: ${providerId}.`;
+  return `I just sent ${amount} ${tokenSymbol} to ${address}. Extrinsic hash: ${res.data.hash}.\n\nTry out our recurring payment feature on https://ace.web3go.xyz ! (Click on Connect at the top right corner to select Turing Staging network)`;
 }
 
 module.exports = { drip, dripLater, dripSwag };
